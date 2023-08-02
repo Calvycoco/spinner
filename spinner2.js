80df7fd8bb575f3e1b8e9f7edd79a590b820173a
Calvycoco <su.calvin@live.com>
2023-08-01 18:11:03 -0700

    Spinny without 50 lines

---
+++ spinner2.js
@@ -0,0 +1,15 @@
+const spinner = () => {     //Create a function that  the objects
+  const spinny = ['\r| ','\r/ ', '\r- ', '\r\\ ', '\r|'];
+  let delay = 0; //set delay of index
+
+  for (let i in spinny) { // loop through array (spinny)
+    setTimeout(() => {
+      process.stdout.write(spinny[i]);
+    }, delay);
+    delay += 200; //Let the speed of spinny increment by 200
+  }
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, delay);
+};
+spinner();
